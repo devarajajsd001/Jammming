90f0a2bebb51c9a2fa30bf13e300b60754b4d6ec
devaraja <devarajajsd001@gmail.com>
2022-04-12 11:02:15 +0530

    music app

---
+++ my-app/src/LoginForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class LoginForm extends React.Component{
+  render(){
+  return (
+        <div className=" loginForm">
+          Krishna
+        </div>
+        );
+      }
+}
+
+export default LoginForm;
